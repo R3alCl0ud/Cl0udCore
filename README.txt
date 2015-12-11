cd0ab4c59e0d377a7f9293f0f7a9f516d05189f4
Perry Berman <superstargamesnetwork@gmail.com>
2015-12-11 14:02:13 -0700

    started project

---
+++ README.txt
@@ -0,0 +1,8 @@
+--Hermitcraft Core mod--
+To setup workspace run 
+gradlew[.bat] setupDevWorkspace, then
+gradlew[.bat] eclipse|idea|prefered IDE
+
+For use in Hermitcraft Explorers modpack
+
+Report bugs in issues tab
---
+++ src/cl0udcore/Cl0udCore.java
@@ -0,0 +1,13 @@
+package cl0udcore;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = Cl0udCore.MODID, name = Cl0udCore.NAME, version = Cl0udCore.VERSION)
+public class Cl0udCore 
+{
+
+	public static final String MODID = "cl0udcore";
+	public static final String NAME = "Cl0udCore";
+	public static final String VERSION = "1.0.0";
+	
+}
