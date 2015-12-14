2ef38dc512fcfda1facbbe929b2116b8665c0210
Perry Berman <RealCloudCraft@users.noreply.github.com>
2015-12-14 10:01:13 -0700

    Create IProxy.java

---
+++ src/cl0udcore/proxy/IProxy.java
@@ -0,0 +1,12 @@
+package cl0udcore.proxy;
+
+import net.minecraft.entity.player.EntityPlayer;
+
+public interface IProxy 
+{
+    void registerKeyBinds();
+    void registerRenderers();
+    void registerClientOnlyEvents();
+	  EntityPlayer getClientPlayer();
+	  boolean isJumpPressed();
+}
